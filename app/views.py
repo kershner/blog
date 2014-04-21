bf2befc8a1bae455ed2bc823686f8091e782c255
Tyler Kershner <tylerkershner@gmail.com>
2014-04-21 16:06:18 -0400

    Day 1 - fully functioning but barebones site

--- app/views.py
+++ app/views.py
@@ -12,3 +12,9 @@ def index():
 def about():
     return render_template("about.html",
                            title="About Me")
+
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html",
+                           title="Projects")
