f582b20433e3767cc1e309285326de0008f367f5
Tyler Kershner <tylerkershner@gmail.com>
2014-04-21 14:29:37 -0400

    First commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html",
+                           title="Home")
+
+@app.route('/about')
+def about():
+    return render_template("about.html",
+                           title="About Me")
