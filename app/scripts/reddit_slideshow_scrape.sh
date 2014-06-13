9fe95eb8ae6ec0e5621495a53b0714fb76d002a5
Tyler Kershner <tylerkershner@gmail.com>
2014-06-13 14:15:37 -0400

    Removed unnecessary bash script

--- app/scripts/reddit_slideshow_scrape.sh
+++
@@ -1,7 +0,0 @@
-python /home/tylerkershner/app/scripts/scrape_aww_gifs.py
-python /home/tylerkershner/app/scripts/scrape_highqualitygifs.py
-python /home/tylerkershner/app/scripts/scrape_interestinggifs.py
-python /home/tylerkershner/app/scripts/scrape_naturegifs.py
-python /home/tylerkershner/app/scripts/scrape_perfectloops.py
-python /home/tylerkershner/app/scripts/scrape_spacegifs.py
-python /home/tylerkershner/app/scripts/scrape_surrealgifs.py
