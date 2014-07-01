0b61beb313e337b8b9b255cc27d8c6cf397ff94b
Tyler Kershner <tylerkershner@gmail.com>
2014-07-01 16:20:05 -0400

    Another tweak to scraper, think we got it this time

--- app/scripts/scrape_reddit.py
+++ app/scripts/scrape_reddit.py
@@ -1,19 +1,17 @@
 import praw
 import sys
-import urllib
+import requests
 from datetime import datetime
 
 
 # Function to determine size of URL via HTML header data
-def getsize(uri):
-    image_file = urllib.urlopen(uri)
-    size = image_file.headers.get("content-length")
-    if size is None:
-        image_file.close()
+def getsize(url):
+    try:
+        r = requests.get(url, stream=True)
+        size = int(r.headers['content-length'])
+        return size
+    except KeyError:
         return 'None'
-    else:
-        image_file.close()
-        return int(size)
 
 
 # Logs files being added and total number of GIFs to /reddit_scraper_log.txt
@@ -66,7 +64,7 @@ large_urls_list = list(large_urls_file)
 for submission in submissions:
     # Skip the URL if there is an error with the connection
     try:
-        r = urllib.urlopen(submission.url)
+        r = requests.get(submission.url)
     except:
         print 'Error requesting %s, skipping...' % submission.url
         continue
@@ -88,14 +86,14 @@ for submission in submissions:
         bad_urls += 1
         continue
     # 404 status code is a broken link
-    if r.getcode() == 404:
+    if r.status_code == 404:
         print '%s is a broken link (404), skipping...' % submission.url
         # Logging bad URL
         bad_urls_file.write(str(submission.url) + '\n')
         bad_urls += 1
         continue
     # 302 is redirection, meaning bad link
-    if r.getcode() == 302:
+    if r.status_code == 302:
         print '%s is a broken link (302), skipping...' % submission.url
         # Logging bad URL
         bad_urls_file.write(str(submission.url) + '\n')
@@ -107,21 +105,20 @@ for submission in submissions:
         bad_urls_file.write(str(submission.url) + '\n')
         bad_urls += 1
         continue
+    # If the getsize function returned None, there was an error
+    if getsize(submission.url) == 'None':
+        print '%s has no length data in HTTP header, skipping...' % submission.url
+        bad_urls_file.write(str(submission.url) + '\n')
+        bad_urls += 1
+        continue
     # The Pi has a hard time with GIFs larger than 8MBs
-    if getsize(submission.url) > 8192000:
+    elif getsize(submission.url) > 8192000:
         print '%s is larger than 8MBs, skipping...' % submission.url
-        large_urls_file.write(str(submission.url) + '\n')
-        large_urls += 1
         continue
     # Imgur 'removed' image is 503 bytes
     elif getsize(submission.url) == 503:
         print '%s is a broken link (503 bytes), skipping...' % submission.url
         urls_file.write(str(submission.url) + '\n')
-        bad_urls += 1
-        continue
-    elif getsize(submission.url) == 'None':
-        print '%s has no length data in HTTP header, skipping...' % submission.url
-        bad_urls_file.write(str(submission.url) + '\n')
         bad_urls += 1
         continue
     else:
--- app/scripts/test.py
+++ app/scripts/test.py
@@ -1,28 +1,17 @@
-import praw
 import requests
-import urllib
 
-file_object = open('E:/programming/projects/blog/app/templates/pi_display/test.txt', 'r')
-file_object_list = list(file_object)
-
-
-def getsize(uri):
-    image_file = urllib.urlopen(uri)
-    size = image_file.headers.get("content-length")
-    if size is None:
-        image_file.close()
+# Function to determine size of URL via HTML header data
+def getsize(url):
+    try:
+        r = requests.get(url, stream=True)
+        size = r.headers['content-length']
+        return int(size)
+    except KeyError:
         return 'None'
-    else:
-        image_file.close()
-        return (int(size) / 1024) / 1024
 
-count = 0
-
-for url in file_object_list:
-    if getsize(url) is 'None':
-        count += 1
-
-print '%d out of %d' % (count, len(file_object_list))
-
-
-file_object.close()
+this_url = 'http://i.minus.com/ibhtxs1tWpNcqY.gif'
+if getsize(this_url) == 'None':
+    print 'Error in this shit'
+else:
+    print 'Gif is larger than 8MBs'
+    print getsize(this_url)
