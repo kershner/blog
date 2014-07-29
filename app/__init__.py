8cf19962c432ac6bdae4934b8f505724c59c3e46
Tyler Kershner <tylerkershner@gmail.com>
2014-07-29 02:00:21 +0000

    Bunch of CSS tweaks/cleanups

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
 app = Flask(__name__)  # Creating Flask Object
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
 app.secret_key = '65413684f65a446g568465d4fv3xc2vsadf5fsadfvxzcvsdf65465421346543654213sd23f1sad3f21sad3f21sad12vb3vx'
-
-from app import routes_dev, models
