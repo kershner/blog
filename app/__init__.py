91ce9f087d288ec6b8ab6ffae3f977d9453ed246
Tyler Kershner <tylerkershner@gmail.com>
2014-12-18 11:52:59 -0500

    Swapping out CSRF dev key

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ import flask_sqlalchemy
 
 app = Flask(__name__)  # Initialize Flask Object
 app.config.from_object('config')
-app.secret_key = 'development key'
 db = flask_sqlalchemy.SQLAlchemy(app)  # Initialize database
 
 from app import routes, models
