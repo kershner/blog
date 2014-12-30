084c1a203348526ffb4f7835a919dff6b98f2d3a
Tyler Kershner <tylerkershner@gmail.com>
2014-12-30 14:26:35 -0500

    Refactored my reddit scraper/URL cleanup scripts

--- app/scripts/clean_up_urls.py
+++ app/scripts/clean_up_urls.py
@@ -1,155 +1,109 @@
-import urllib2
+import requests
 from datetime import datetime
 
 
 class Log(object):
-    def __init__(self, count, bad_urls, large_urls):
-        self.count = count
-        self.bad_urls = bad_urls
-        self.large_urls = large_urls
+    def __init__(self, clean_urls_all, bad_urls_all, clean_urls_animals, bad_urls_animals, clean_urls_gaming,
+                 bad_urls_gaming, clean_urls_strange, bad_urls_strange, clean_urls_educational,
+                 bad_urls_educational):
+        self.clean_urls_all = clean_urls_all
+        self.bad_urls_all = bad_urls_all
+        self.clean_urls_animals = clean_urls_animals
+        self.bad_urls_animals = bad_urls_animals
+        self.clean_urls_gaming = clean_urls_gaming
+        self.bad_urls_gaming = bad_urls_gaming
+        self.clean_urls_strange = clean_urls_strange
+        self.bad_urls_strange = bad_urls_strange
+        self.clean_urls_educational = clean_urls_educational
+        self.bad_urls_educational = bad_urls_educational
 
-    def counter(self):
-        self.count += 1
+    def counter(self, list_type, url_type):
+        if url_type == 'clean':
+            if list_type == 'all_urls':
+                self.clean_urls_all += 1
+            elif list_type == 'animals_urls':
+                self.clean_urls_animals += 1
+            elif list_type == 'gaming_urls':
+                self.clean_urls_gaming += 1
+            elif list_type == 'strange_urls':
+                self.clean_urls_strange += 1
+            elif list_type == 'educational_urls':
+                self.clean_urls_educational += 1
+        else:
+            if list_type == 'all_urls':
+                self.bad_urls_all += 1
+            elif list_type == 'animals_urls':
+                self.bad_urls_animals += 1
+            elif list_type == 'gaming_urls':
+                self.bad_urls_gaming += 1
+            elif list_type == 'strange_urls':
+                self.bad_urls_strange += 1
+            elif list_type == 'educational_urls':
+                self.bad_urls_educational += 1
 
-    def bad_urls_counter(self):
-        self.bad_urls += 1
+    def readout(self):
+        print '\nStats:'
+        print 'Added %d clean URLs to all_urls.txt' % self.clean_urls_all
+        print 'Removed %d bad URLs from all_urls.txt' % self.bad_urls_all
+        print '\nAdded %d clean URLs to animals_urls.txt' % self.clean_urls_animals
+        print 'Removed %d bad URLs from animals_urls.txt' % self.bad_urls_animals
+        print '\nAdded %d clean URLs to gaming_urls.txt' % self.clean_urls_gaming
+        print 'Removed %d bad URLs from gaming_urls.txt' % self.bad_urls_gaming
+        print '\nAdded %d clean URLs to strange_urls.txt' % self.clean_urls_strange
+        print 'Removed %d bad URLs from strange_urls.txt' % self.bad_urls_strange
+        print '\nAdded %d clean URLs to educational_urls.txt' % self.clean_urls_educational
+        print 'Removed %d bad URLs from educational_urls.txt' % self.bad_urls_educational
 
-    def large_urls_counter(self):
-        self.large_urls += 1
 
-# Instantiating Log class
-log = Log(0, 0, 0)
-
-
-def clean_up_urls(path, urls_file):
-    log.count = 0
-    log.bad_urls = 0
-    log.large_urls = 0
-    print '\nBeginning cleanup of %s\n' % urls_file
-
-    # Function to determine size of URL via HTTP header data
-    def getsize(image_url):
-        try:
-            image_url = urllib2.urlopen(image_url)
-            size = int(image_url.info()['content-length'])
-            return size
-        except(KeyError, urllib2.HTTPError, urllib2.URLError):
-            return 'None'
-
+def clean_up_urls(path, filename):
+    clean_urls = []
     url_number = 0
-
-    # Opening files, converting to Python lists
-    with open('%s/%s' % (path, urls_file), 'a+') as temp_file:
-        urls_list = list(temp_file)
-    bad_urls_file = open('%s/bad_urls.txt' % path, 'a+')
-    bad_urls_list = list(bad_urls_file)
-    large_urls_file = open('%s/large_urls.txt' % path, 'a+')
-    large_urls_list = list(large_urls_file)
-    clean_urls_file = open('%s/clean_urls.txt' % path, 'a+')
-
-    for url in urls_list:
-        end_point = url.find('\n')
-        url_test = url[:end_point]
+    list_type = filename[:filename.find('.txt')]
+    with open('%s/%s' % (path, filename), 'a+') as f:
+        urls_list = list(f)
+    for image_url in urls_list:
+        end_point = image_url.find('\n')
+        url_test = image_url[:end_point]
         url_number += 1
         print 'URL %d of %d' % (url_number, len(urls_list))
-        # Already in urls.txt
-        if url + '\n' in bad_urls_list:
-            log.bad_urls_counter()
+
+        if image_url in bad_urls_list:
+            log.counter(list_type, 'bad')
             continue
-        # Skipping GFYCat URLs for now
-        elif 'gfycat' in url:
-            print 'GFYCat link, skipping for now...'
-            clean_urls_file.write(str(url))
-            continue
-        # Known Large URL
-        elif url + '\n' in large_urls_list:
-            log.large_urls_counter()
-            continue
-        # Not a .gif file
         elif not url_test.endswith('.gif'):
-            print '%s is not a GIF file, skipping...' % url
-            bad_urls_file.write(str(url))
-            log.bad_urls_counter()
-            continue
-        # Don't want gifsound links
-        elif 'sound' in url:
-            print '%s is a gifsound link, skipping...' % url
-            bad_urls_file.write(str(url))
-            log.bad_urls_counter()
-            continue
-        try:
-            r = urllib2.urlopen(url)
-        except (urllib2.HTTPError, urllib2.URLError):
-            print '%s returns 403 forbidden or timed out, skipping...' % url
-            bad_urls_file.write(str(url))
-            log.bad_urls_counter()
-            continue
-        # 404 status code is a broken link
-        if r.getcode == 404:
-            print '%s is a broken link (404), skipping...' % url
-            # Logging bad URL
-            bad_urls_file.write(str(url))
-            log.bad_urls_counter()
-            continue
-        # 302 is redirection, meaning bad link
-        elif r.getcode == 302:
-            print '%s is a broken link (302), skipping...' % url
-            # Logging bad URL
-            bad_urls_file.write(str(url))
-            log.bad_urls_counter()
-            continue
-        # The Pi has a hard time with GIFs larger than 8MBs
-        if getsize(url) > 8192000:
-            print '%s is larger than 8MBs, skipping...' % url
-            large_urls_file.write(str(url))
-            log.large_urls_counter()
-            continue
-        # Imgur 'removed' image is 503 bytes
-        elif getsize(url) == 503:
-            print '%s is a broken link (503 bytes), skipping...' % url
-            bad_urls_file.write(str(url))
-            log.bad_urls_counter()
-            continue
-        # If the getsize function returned None, there was an error
-        elif getsize(url) == 'None':
-            print '%s has no length data in HTTP header, skipping...' % url
-            bad_urls_file.write(str(url))
-            log.bad_urls_counter()
+            print '%s is not a .gif file, skipping...' % url_test
+            log.counter(list_type, 'bad')
             continue
         else:
-            print 'Clean URL, adding...'
-            log.counter()
-            clean_urls_file.write(str(url))
+            r = requests.get(url_test, stream=True)
+            try:
+                size = int(r.headers['content-length'])
+                if size == 503:
+                    print '%s is a broken link (503 bytes), skipping...' % url_test
+                    log.counter(list_type, 'bad')
+                    continue
+            except KeyError:
+                clean_urls.append(image_url)
+            code = r.status_code
+            if code == 404:
+                print '%s is a broken link (404), skipping...' % url_test
+                log.counter(list_type, 'bad')
+                continue
+            elif code == 302:
+                print '%s is a broken link (302), skipping...' % url_test
+                log.counter(list_type, 'bad')
+                continue
+            else:
+                log.counter(list_type, 'clean')
+                clean_urls.append(image_url)
 
-    # Closing files
-    bad_urls_file.close()
-    large_urls_file.close()
-    clean_urls_file.close()
+    # Open/close file in write mode to erase it
+    open('%s/%s' % (path, filename), 'w').close()
 
-    # Creating Python list from newly populated clean_urls.txt
-    with open('%s/clean_urls.txt' % path, 'r') as updated_clean_urls:
-        clean_urls_list = list(updated_clean_urls)
-
-    # Opening/closing urls.txt (taking advantage of side effect to erase contents)
-    open('%s/%s' % (path, urls_file), 'w').close()
-
-    # Re-opening urls.txt, appending with contents of the clean_urls.txt list, closing the file
-    url_file = open('%s/%s' % (path, urls_file), 'a+')
-    for url in clean_urls_list:
-        url_file.write(url)
-    url_file.close()
-
-    # Opening and closing the clean_urls.txt file.  Side effect to erase contents of file.
-    print '\n\n\n\nErasing contents of clean_urls.txt...'
-    open('%s/clean_urls.txt' % path, 'w').close()
-
-    # Opening updated file, printing # of URLs
-    with open('%s/%s' % (current_path, urls_file), 'r') as f:
-        number_of_gifs = len(list(f))
-        print '\nTotal number of GIFS: %d' % number_of_gifs
-    print '%d bad links removed' % log.bad_urls
-    print '%d large GIFs removed' % log.large_urls
-
+    # Write contents of clean_urls list to file
+    with open('%s/%s' % (path, filename), 'a+') as clean_file:
+        for url in clean_urls:
+            clean_file.write(url)
 
 if __name__ == '__main__':
     prompt = raw_input('Are you running this file from work or home? > ').lower()
@@ -158,13 +112,19 @@ if __name__ == '__main__':
     else:
         current_path = 'H:/programming/projects/blog/app/templates/pi_display/logs'
 
-    time_start = str(datetime.now().strftime('%I:%M %p on %A, %B %d, %Y'))
-
     files = ['all_urls.txt', 'animals_urls.txt', 'gaming_urls.txt', 'strange_urls.txt', 'educational_urls.txt']
-    for entry in files:
-        clean_up_urls(current_path, entry)
 
+    with open('%s/%s' % (current_path, 'bad_urls.txt'), 'a+') as temp_file:
+        bad_urls_list = list(temp_file)
+
+    log = Log(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    time_start = str(datetime.now().strftime('%I:%M %p on %A, %B %d, %Y'))
+    for entry in files:
+        print '\n#########################'
+        print 'Beginning clean up of %s...' % entry
+        clean_up_urls(current_path, entry)
+    log.readout()
     time_end = str(datetime.now().strftime('%I:%M %p on %A, %B %d, %Y'))
 
-    print 'URL cleanup began at %s' % time_start
+    print '\nURL cleanup began at %s' % time_start
     print 'URL cleanup finished at %s' % time_end
--- app/scripts/scrape_reddit.py
+++ app/scripts/scrape_reddit.py
@@ -1,151 +1,85 @@
+import requests
+from requests import exceptions
 import praw
-import urllib2
-from datetime import datetime
 
 
-# Creating Gif class to hold count data
-class Gif(object):
-    def __init__(self, count):
-        self.count = count
+class Log(object):
+    def __init__(self, all_gifs, animals_gifs, gaming_gifs, strange_gifs, educational_gifs):
+        self.all_gifs = all_gifs
+        self.animals_gifs = animals_gifs
+        self.gaming_gifs = gaming_gifs
+        self.strange_gifs = strange_gifs
+        self.educational_gifs = educational_gifs
 
-    def counter(self):
-        self.count += 1
+    def gif_counter(self, category):
+        if category == 'all':
+            self.all_gifs += 1
+        elif category == 'animals':
+            self.animals_gifs += 1
+        elif category == 'gaming':
+            self.gaming_gifs += 1
+        elif category == 'strange':
+            self.strange_gifs += 1
+        elif category == 'educational':
+            self.educational_gifs += 1
 
-# Instantiating Gif class
-gif = Gif(0)
+    def readout(self):
+        categories = ['all', 'animals', 'gaming', 'strange', 'educational']
+        numbers = [self.all_gifs, self.animals_gifs, self.gaming_gifs, self.strange_gifs, self.educational_gifs]
+        counter = 0
+        print '\n'
+        for entry in categories:
+            print '%d GIFs added to %s_urls.txt' % (numbers[counter], entry)
+            counter += 1
 
 
 def scrape_reddit(target_subreddit, path, category):
-    # Function to determine size of URL via HTTP header data
-    def getsize(url):
-        try:
-            url = urllib2.urlopen(url)
-        except urllib2.HTTPError:
-            print '404 - skipping'
-            return
-        try:
-            size = int(url.info()['content-length'])
-            return size
-        except KeyError:
-            print '%s has no content-length data in HTTP header, skipping...' % submission.url
-            return 'None'
-
-    # Logs files being added and total number of GIFs to /reddit_scraper_log.txt
-    def log():
-        time = str(datetime.now().strftime('%I:%M %p on %A, %B %d, %Y'))
-        log_data = '\n\n%d gifs added from /r/%s at %s.' % (count, target_subreddit, time)
-        skipped = '\n%d bad links and %d large GIFs skipped.' % (bad_urls, large_urls)
-        number_of_gifs = 'Total number of %s GIFs: %d' % (category, len(urls_list) + count)
-        print log_data
-        print skipped
-        print number_of_gifs
-
-    # Variables to keep track of certain GIFs added
-    count = 0
-    bad_urls = 0
-    large_urls = 0
-
-    print '\nGathering image URLs from /r/%s...' % target_subreddit
+    clean_urls = []
+    print 'Gathering image URLs from /r/%s...' % target_subreddit
 
     # Accessing Reddit API
     r = praw.Reddit(user_agent='Raspberry Pi Project by billcrystals')
+    submissions = r.get_subreddit(target_subreddit).get_hot(limit=200)
 
-    # Uncomment to scrape top results from year/month/all
-    submissions = r.get_subreddit(target_subreddit).get_hot(limit=100)
-    #submissions = r.get_subreddit(target_subreddit).get_top_from_year(limit=50)
-    #submissions = r.get_subreddit(target_subreddit).get_top_from_month(limit=50)
-    #submissions = r.get_subreddit(target_subreddit).get_top_from_all(limit=100)
+    # Creating Python list from url file
+    with open('%s/%s_urls.txt' % (path, category), 'a+') as f:
+        current_urls = list(f)
 
-    # Opening files, converting to Python lists
-    urls_file = open('%s/%s_urls.txt' % (path, category), 'a+')
-    urls_list = list(urls_file)
-    bad_urls_file = open('%s/bad_urls.txt' % path, 'a+')
-    bad_urls_list = list(bad_urls_file)
-    large_urls_file = open('%s/large_urls.txt' % path, 'a+')
-    large_urls_list = list(large_urls_file)
-
-    # Going through reddit submissions from the specified subreddit
     for submission in submissions:
-        try:
-            # Already in urls.txt
-            if str(submission.url) + '\n' in urls_list:
-                continue
-           # Known bad URL
-            elif str(submission.url) + '\n' in bad_urls_list:
-                bad_urls += 1
-                continue
-            # Known Large URL
-            elif str(submission.url) + '\n' in large_urls_list:
-                large_urls += 1
-                continue
-            # Not a .gif file
-            elif not submission.url.endswith('.gif'):
-                print '%s is not a GIF file, skipping...' % submission.url
-                bad_urls += 1
-                continue
-        except UnicodeEncodeError:
-            print 'Unicode error, skipping...'
+        if submission.url + '\n' in current_urls:
             continue
-        # Don't want gifsound links
-        if 'sound' in str(submission.url):
-            print '%s is a gifsound link, skipping...' % submission.url
-            bad_urls_file.write(str(submission.url) + '\n')
-            bad_urls += 1
+        elif not submission.url.endswith('.gif'):
             continue
-        # This URL causes an ASCII -> Unicode error
-        elif 'Von_Karman' in submission.url:
-            bad_urls += 1
-            continue
-        try:
-            r = urllib2.urlopen(submission.url)
-        except (urllib2.HTTPError, urllib2.URLError):
-            bad_urls += 1
-            print '%s returns 403 forbidden or timed out, skipping...' % submission.url
-            bad_urls_file.write(str(submission.url) + '\n')
-            continue
-        # 404 status code is a broken link
-        if r.getcode == 404:
-            print '%s is a broken link (404), skipping...' % submission.url
-            # Logging bad URL
-            bad_urls_file.write(str(submission.url) + '\n')
-            bad_urls += 1
-            continue
-        # 302 is redirection, meaning bad link
-        elif r.getcode == 302:
-            print '%s is a broken link (302), skipping...' % submission.url
-            # Logging bad URL
-            bad_urls_file.write(str(submission.url) + '\n')
-            bad_urls += 1
-            continue
-        # If the getsize function returned None, there was an error
-        if getsize(submission.url) == 'None':
-            print '%s has no length data in HTTP header, skipping...' % submission.url
-            bad_urls_file.write(str(submission.url) + '\n')
-            bad_urls += 1
-            continue
-        # The Pi has a hard time with GIFs larger than 8MBs
-        elif getsize(submission.url) > 8192000:
-            print '%s is larger than 8MBs, skipping...' % submission.url
-            large_urls_file.write(str(submission.url) + '\n')
-            large_urls += 1
-            continue
-        # Imgur 'removed' image is 503 bytes
-        elif getsize(submission.url) == 503:
-            print '%s is a broken link (503 bytes), skipping...' % submission.url
-            bad_urls_file.write(str(submission.url) + '\n')
-            bad_urls += 1
+        elif 'sound' in submission.url:
             continue
         else:
-            print '%s not found in %s_urls.txt, adding...' % (submission.url, entry)
-            urls_file.write(str(submission.url) + '\n')
-            count += 1
-            gif.counter()
+            try:
+                request = requests.get(submission.url, stream=True)
+                code = request.status_code
+            except (requests.exceptions.SSLError, requests.exceptions.ConnectionError):
+                continue
+            try:
+                size = int(request.headers['content-length'])
+                if size == 503:
+                    continue
+            except KeyError:
+                clean_urls.append(submission.url + '\n')
+                log.gif_counter(category)
+            if code == 404:
+                continue
+            elif code == 302:
+                continue
+            else:
+                clean_urls.append(submission.url + '\n')
+                log.gif_counter(category)
 
-    urls_file.close()
-    bad_urls_file.close()
-    large_urls_file.close()
-
-    log()
+    # Appending contents of clean_urls to current url file
+    with open('%s/%s_urls.txt' % (path, category), 'a+') as f:
+        for line in clean_urls:
+            try:
+                f.write(line)
+            except UnicodeEncodeError:
+                continue
 
 if __name__ == '__main__':
     prompt = raw_input('Are you scraping from work or home? > ').lower()
@@ -169,6 +103,7 @@ if __name__ == '__main__':
         ['physicsgifs', 'educationalgifs', 'chemicalreactiongifs', 'interestinggifs']
     ]
 
+    log = Log(0, 0, 0, 0, 0)
     count = 0
     for entry in categories:
         print '\n####################################'
@@ -176,6 +111,5 @@ if __name__ == '__main__':
         print '####################################'
         for subreddit in subreddits[int('%d' % count)]:
             scrape_reddit(subreddit, current_path, entry)
-        print '%d GIFs added to %s_urls.txt.' % (gif.count, entry)
-        gif.count = 0
         count += 1
+    log.readout()
