d0cd8f04856b28cc3b5788fa85c06e77aa0e68c1
Tyler Kershner <tylerkershner@gmail.com>
2015-09-21 14:41:30 +0000

    New settings for Pi display

--- app/static/js/pi_display.js
+++ app/static/js/pi_display.js
@@ -1,8 +1,17 @@
+var COLORS = ['#25B972', '#ff6767', '#FFA533', '#585ec7', '#FF8359'];
+
 function getImage() {
     clearInterval(window.delay);
-    $('.container img').one("transitionend",
+    $('.container img').one("webkitTransitionEnd",
       function(event) {
         $(this).remove();
+        $('.loading').colorWave(COLORS);
+        setTimeout(function() {
+            $('.loading').colorWave(COLORS);
+            setTimeout(function() {
+                $('.loading').colorWave(COLORS);
+            }, 3000);
+        }, 3000);
     });
     $('.container img').css('opacity', '0');
     $.ajax({
@@ -24,3 +33,58 @@ function getImage() {
     });
     return false;
 };
+
+(function( $ ) {
+  $.fn.colorWave = function(colors) {
+    function _colorWave(colors, element) {
+      var finalHtml = '';
+      var text = $(element).text();
+      var defaultColor = $(element).css('color');
+      var wait = text.length * 350;
+      // Placing <spans> around each letter with class="colorwave"
+      var tempHtml = '';
+      for (i=0; i<text.length; i++) {
+          tempHtml = '<span class="colorwave">' + text[i] + '</span>';
+          finalHtml += tempHtml;
+      }
+      $(element).empty().append(finalHtml);
+      _colorLetters(colors, element, wait, defaultColor);
+    }
+    // Iterates through given color array, applies color to a colorwave span
+    function _colorLetters(colors, element, wait, defaultColor) {
+        var randomnumber = (Math.random() * (colors.length + 1)) << 0;
+        var counter = randomnumber;
+        var delay = 100;
+        var adjustedWait = wait / 5;
+        $(element).find('.colorwave').each(function() {
+            if (counter >= colors.length) {
+                counter = 0;
+            }
+            $(this).animate({
+                'color': colors[counter],
+                'bottom': '+=6px'
+                }, delay);
+            delay += 75;
+            counter += 1;
+        });
+        setTimeout(function() {
+            _removeColor(element, defaultColor);
+        }, adjustedWait);
+    }
+    // Iterates through color wave spans, returns each to default color
+    function _removeColor(element, defaultColor) {
+        var delay = 100;
+        $(element).find('.colorwave').each(function() {
+            $(this).animate({
+                'color': defaultColor,
+                'bottom': '-=6px'
+            }, delay);
+            delay += 75;
+        });
+    }
+    return this.each(function() {
+      _colorWave(colors, this);
+    });
+    return this;
+  }
+}( jQuery ));
