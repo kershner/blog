77e3283baef7bd6735f46d6ab578174db319d9ce
Tyler Kershner <tylerkershner@gmail.com>
2016-04-14 18:17:57 +0000

    One final small bug fix

--- app/scripts/clean_up_urls.py
+++ app/scripts/clean_up_urls.py
@@ -93,9 +93,9 @@ def load_url(gif_url):
             log.removed_gifs.append(logged_gif)
         else:
             # print '%d | %d GIFs remaining | %s' % (code, len(log.gif_list), gif_url)
-            continue
+            pass
     except Exception as e:
-        continue
+        pass
         # print '\n\n@@@@@@@@@@@@@@@@@@'
         # print 'ERROR - %s | %s' % (e.message, gif_url)
         # logged_gif = {
