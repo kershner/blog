b3461fcae192137acbdd176957b0c3c4c98da7e8
Tyler Kershner <tylerkershner@gmail.com>
2016-06-07 23:48:28 +0000

    Fixed bug related to trying to play a deleted gif

--- app/modules/pi_display/display.py
+++ app/modules/pi_display/display.py
@@ -12,25 +12,18 @@ def grab_gif_data():
     print '%d URLs remaining...' % len(gif_ids_to_play)
 
     if not config.gif_ids_to_play:
-        print 'Generating new list of gif IDs...'
-        if config.active_tags:
-            new_gif_ids_list = pi_config.get_new_gif_ids_list(config.active_tags.split(','))
-            config.gif_ids_to_play = new_gif_ids_list
-        else:
-            new_gif_ids_list = [str(gif.id) for gif in models.Gif.query.all()]
-            config.gif_ids_to_play = ','.join(new_gif_ids_list)
+        gif_ids_to_play = get_new_gif_list()
 
-        gif_ids_to_play = new_gif_ids_list
-
-    choice = random.choice(gif_ids_to_play)
-    gif_ids_to_play.remove(choice)
+    gif = None
+    while gif is None:
+        choice = random.choice(gif_ids_to_play)
+        gif_ids_to_play.remove(choice)
+        gif = models.Gif.query.filter_by(id=choice).first()
 
     config.gif_ids_to_play = ','.join([str(gif_id) for gif_id in gif_ids_to_play])
-
-    gif = models.Gif.query.filter_by(id=choice).first()
-    gif.last_played = datetime.now()
-
     config.current_gif = gif.url
+
+    gif.last_played = datetime.now()
 
     db.session.add(gif)
     db.session.add(config)
@@ -42,3 +35,16 @@ def grab_gif_data():
     }
 
     return data
+
+
+def get_new_gif_list():
+    config = models.Config.query.first()
+    print 'Generating new list of gif IDs...'
+    if config.active_tags:
+        new_gif_ids_list = pi_config.get_new_gif_ids_list(config.active_tags.split(','))
+        config.gif_ids_to_play = new_gif_ids_list
+    else:
+        new_gif_ids_list = [str(gif.id) for gif in models.Gif.query.all()]
+        config.gif_ids_to_play = ','.join(new_gif_ids_list)
+
+    return new_gif_ids_list
